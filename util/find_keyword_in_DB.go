cf33626070f68d9cf07df3549657fb071ce6ac32
Alexey Kashuba <alexey.kashuba97@gmail.com>
2019-01-13 20:06:49 +0300

    util

---
+++ util/find_keyword_in_DB.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"flag"
+	"github.com/boltdb/bolt"
+	"github.com/mvidigueira/Peerster/dht"
+	"github.com/mvidigueira/Peerster/dht_util"
+	"github.com/reiver/go-porterstemmer"
+	"log"
+)
+
+func FindKeyword(name string, keyword string) {
+	db, err := bolt.Open(name, 0666, nil)
+	if err != nil {
+		panic(err)
+	}
+	keyword = porterstemmer.StemString(keyword)
+	id := dht_util.GenerateKeyHash(keyword)
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(dht.KeywordsBucket))
+		resultData := b.Get(id[:])
+		log.Println(len(resultData))
+		return nil
+	})
+}
+
+func main(){
+	keyword := flag.String("keyword", "", "keyword to find")
+	db := flag.String("name", "", "name of DB")
+	flag.Parse()
+	FindKeyword(*db, *keyword)
+}
